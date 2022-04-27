44c858f94188eeab567713febf077bd6e35baf03
Ashwin Kambli <ashwin.kambli@hotmail.com>
2022-04-27 15:34:31 -0700

    Added BinaryTree

---
+++ BinaryTree.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class BinaryTree
+    {
+        private Node _root; 
+        public BinaryTree()
+        {
+            _root = null;
+        }
+
+        public void Add(int val)
+        {
+            if(_root == null)
+            {
+                _root = new Node(val);
+                return;
+            }
+
+            Node current = _root;
+            Node parent = null; 
+            while(current != null)
+            {
+                if(val < current.Value)
+                {
+                    parent = current;
+                    current = current.Left; 
+                }
+                else
+                {
+                    parent = current;
+                    current = current.Right; 
+                }
+            }
+            Node node = new Node(val);
+            if(val < parent.Value)
+            {
+                parent.Left = node;
+            }
+            else
+            {
+                parent.Right = node; 
+            }
+        }
+
+        public bool Search(int searchVal)
+        {
+            Node current = _root; 
+            while(current != null)
+            {
+                if (current.Value == searchVal)
+                    return true;
+
+                if (searchVal < current.Value)
+                    current = current.Left;
+                else
+                    current = current.Right; 
+            }
+            return false; 
+        }
+
+        public void Delete(int val)
+        {
+            if (_root == null)
+            {
+                Console.WriteLine($"Tree is empty. Cannot delete {val}");
+                return;
+            }
+
+            _root = DeleteRecursive(_root, val);
+                return; 
+        }
+
+        private Node DeleteRecursive(Node root, int val)
+        {
+            if(root == null)
+                return null;
+
+            if(val < root.Value)
+                root.Left = DeleteRecursive(root.Left, val);    
+            else if(val > root.Value)
+                root.Right = DeleteRecursive(root.Right, val);
+            else //Match found
+            {
+                if (root.Left == null && root.Right == null) //Leaf Node
+                    return null;
+                else if(root.Left == null && root.Right != null)
+                {
+                    return root.Right; 
+                }
+                else if(root.Right == null && root.Left != null)
+                {
+                    return root.Left; 
+                }
+                else
+                {
+                    //Not leaf and has both sub-trees.
+                    //TODO: Find max in left subtree and assign that ehre and delete the left max
+                    return null; 
+                }
+            }
+            return root; 
+        }
+
+        public void Print()
+        {
+             PrintRecurive(_root);
+        }
+
+        private void PrintRecurive(Node node)
+        {
+            if (node.Left != null)
+                PrintRecurive(node.Left);
+
+            Console.WriteLine($"{node.Value}, ");
+
+            if(node.Right != null)
+                PrintRecurive(node.Right);
+        }
+    }
+
+    public class Node
+    {
+        private int _val;
+        private Node _left;
+        private Node _right; 
+        public Node(int val)
+        {
+            _val = val;
+            _left = null; 
+            _right = null; 
+        }
+
+        public Node Left {  get { return _left; } set { _left = value; } }
+        public Node Right { get { return _right; } set { _right = value; } }
+        public int Value { get { return _val; } }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using CSharp_Scratchpad;
+using ConsoleApp1;
 
-new USStatesMissingLetter().FindmissingLetters().ForEach(letter => Console.Write($"{letter}, "));
+//Console.WriteLine(new DirectoryComparer().Compare("C:\temp", "C:\temp1"));
+
+BinaryTree bt = new BinaryTree();
+bt.Add(3);
+bt.Add(31);
+bt.Add(2);
+bt.Add(23);
+bt.Add(15);
+bt.Add(9);
+
+bt.Add(1);
+bt.Add(-22);
+bt.Add(-8);
+bt.Add(5);
+bt.Add(99);
+
+bt.Print();
+
+Console.WriteLine("Tree Contains 2: {0}", bt.Search(2));
+Console.WriteLine("Tree Contains 25: {0}", bt.Search(25));
